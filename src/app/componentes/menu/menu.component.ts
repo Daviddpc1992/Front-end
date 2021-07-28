4283f6f0904acd023a766036c915a0d4cb7e0a41
David <daviddelpozocalderon@gmail.com>
2021-07-28 18:26:53 +0200

    fixed

--- src/app/componentes/menu/menu.component.ts
+++ src/app/componentes/menu/menu.component.ts
@@ -6,20 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  showMenu: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-  onClick($event) {
-    let controlName = $event.target.getAttribute("class");
-    console.log($event)
-    console.log($event)
-    if ($event) {
-      this.showMenu[controlName] = true;
-   
-    } else {
-      this.showMenu[controlName] = false;
-    }
-  }
+
 }
