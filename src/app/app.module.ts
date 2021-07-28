af69cccf58094c5cae8d1aa55ee27176d0c2f078
David <daviddelpozocalderon@gmail.com>
2021-07-28 18:18:54 +0200

    repasar estilos y mejorar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,6 @@ import { TrackingComponent } from './componentes/tracking/tracking.component';
 import { EnvioComponent } from './componentes/envio/envio.component';
 import { MenuComponent } from './componentes/menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatMenuModule} from '@angular/material/menu';
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatListModule } from '@angular/material/list';
-import {MatExpansionModule} from '@angular/material/expansion'
 
 @NgModule({
   declarations: [
@@ -22,10 +18,7 @@ import {MatExpansionModule} from '@angular/material/expansion'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSidenavModule,
-    MatMenuModule,
-    MatListModule,
-    MatExpansionModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
